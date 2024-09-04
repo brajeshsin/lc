92b96bbeac3993db5147224e4feca829065f8b95
brajeshsin <brajeshsingh2911@gmail.com>
2024-09-05 03:03:27 +0530

    add link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,9 +11,21 @@ const Footer = () => {
    <div className = "max-w-screen-2xl container mx-auto px-4 md:px-20 mt-10">
     <div className='flex flex-col items-center justify-center'>
         <div className='flex space-x-5'>
-        <AiFillInstagram size={24} className='hover:cursor-pointer' />
-        <BsTwitterX size={24}/>
-        <FaLinkedin size={24}/>
+        <ul className='flex space-x-5'>
+        <li>   
+        <a href="https://www.instagram.com/__brajesh_singh__?igsh=MXByMXBhZTE3c2p2cQ==">
+        <AiFillInstagram size={35} className='hover:cursor-pointer hover:scale-110 duration-300' />
+        </a>
+        </li>
+        <li>
+        <BsTwitterX size={24}  className='hover:cursor-pointer hover:scale-110 duration-300' />
+        </li>
+        <li>
+        <a href="https://www.linkedin.com/in/brajesh-kumar-singh-328413229/">
+        <FaLinkedin size={24}  className='hover:cursor-pointer hover:scale-110 duration-300' />
+        </a>
+        </li>
+        </ul>
         </div>
         <div className='mt-8 border-t border-gray-700 pt-8 flex flex-col items-center'>
         <p className='text-sm'>&copy;2024 Your Company. All rights reserved.</p>
